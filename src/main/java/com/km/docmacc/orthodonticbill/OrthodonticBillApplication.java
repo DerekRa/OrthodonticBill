e1a56024d14b98afb63c94c170a35f794af6dee1
KenMac <armstrong17.km@gmail.com>
2025-01-28 14:41:46 +0800

    Added skeletone pom and properties

---
+++ src/main/java/com/km/docmacc/orthodonticbill/OrthodonticBillApplication.java
@@ -0,0 +1,13 @@
+package com.km.docmacc.orthodonticbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrthodonticBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrthodonticBillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/km/docmacc/orthodonticbill/OrthodonticBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.km.docmacc.orthodonticbill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrthodonticBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
